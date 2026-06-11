5efd6560e6847a256912e492de8233c709910294
Rodrigo Borges <digoborges999@gmail.com>
2026-06-11 20:34:42 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 
- const testRoutes = require("./routes/testRoutes")
+const generoRoutes = require("./routes/generoRoutes")
+const testRoutes = require("./routes/testRoutes")
+const filmeRoutes = require("./routes/filmeRoutes")
 
 const app = express()
 
@@ -10,6 +12,8 @@ app.get("/",(req,res) => {
     return res.send("API DA LOCADORA FUNCIONANDO")
 })
 
- app.use("/test",testRoutes)
+app.use("/test",testRoutes)
+app.use("/generos",generoRoutes)
+app.use("filmes",filmeRoutes)
 
 module.exports = app
